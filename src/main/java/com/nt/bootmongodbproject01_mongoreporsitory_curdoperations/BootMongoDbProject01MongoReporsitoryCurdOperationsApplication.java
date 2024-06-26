c97b96c122299afc897edfc4ba49c97bf1c220b0
anand8533 <161494555+anand8533@users.noreply.github.com>
2024-06-26 14:39:32 -0400

    1
    1

---
+++ src/main/java/com/nt/bootmongodbproject01_mongoreporsitory_curdoperations/BootMongoDbProject01MongoReporsitoryCurdOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.nt.bootmongodbproject01_mongoreporsitory_curdoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMongoDbProject01MongoReporsitoryCurdOperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMongoDbProject01MongoReporsitoryCurdOperationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nt/bootmongodbproject01_mongoreporsitory_curdoperations/BootMongoDbProject01MongoReporsitoryCurdOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.bootmongodbproject01_mongoreporsitory_curdoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMongoDbProject01MongoReporsitoryCurdOperationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
