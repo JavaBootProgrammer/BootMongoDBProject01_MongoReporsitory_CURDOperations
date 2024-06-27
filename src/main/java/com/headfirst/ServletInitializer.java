73eecc2e42796846fb48ed3a37355563e80a5467
apasunoori <anand8533@gmail.com>
2024-06-27 15:14:17 -0400

    4

--- src/main/java/com/headfirst/ServletInitializer.java
+++ src/main/java/com/headfirst/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BootMongoDbProject01MongoReporsitoryCurdOperationsApplication.class);
+		return application.sources(BootMongoDbProject01MongoRepositoryCurdOperationsApplication.class);
 	}
 
 }
